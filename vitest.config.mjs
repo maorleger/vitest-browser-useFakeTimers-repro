aa2d5b5f20c6ce6b4e88182abc9553daede4aa9f
Maor Leger <maorleger@microsoft.com>
2024-01-16 10:55:39 -0800

    upgrade to 1.2.0 to repro, disable watch mode

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    watch: false,
     browser: {
       enabled: true,
-      headless: false,
+      headless: true,
       name: "chromium",
       provider: "playwright",
     },
